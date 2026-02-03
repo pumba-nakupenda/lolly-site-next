d3304c8ee7786089fce8c122269cbc0bf9964f47
pumba-nakupenda <oudama@lolly.sn>
2026-02-03 01:37:28 +0000

    Feat: Integrate Sanity Studio at /studio route

---
+++ src/app/studio/[[...tool]]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../../../studio/sanity.config";
+
+export const dynamic = "force-static";
+
+export { viewport } from "next-sanity/studio";
+export { metadata } from "next-sanity/studio/metadata";
+
+export default function StudioPage() {
+    return <NextStudio config={config} />;
+}
